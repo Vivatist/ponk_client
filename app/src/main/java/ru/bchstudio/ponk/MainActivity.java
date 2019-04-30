7aa0d46c31ecaa337855f4db7efb2f3f2a362d97
Андрей Бочкарев <anvabo@gmail.com>
2019-04-30 12:16:12 +0300

    Вынесли notification в отдельный класс

--- app/src/main/java/ru/bchstudio/ponk/MainActivity.java
+++ app/src/main/java/ru/bchstudio/ponk/MainActivity.java
@@ -1,7 +1,6 @@
 package ru.bchstudio.ponk;
 
 import android.app.LauncherActivity;
-import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
@@ -11,18 +10,14 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.net.Uri;
 import android.os.Build;
-import android.support.annotation.RequiresApi;
 import android.support.v4.app.NotificationCompat;
-import android.support.v4.app.NotificationManagerCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 
 
@@ -31,12 +26,10 @@ public class MainActivity extends AppCompatActivity implements OnWebAsyncTaskCom
 
 
     private static final String myURL = "http://89.169.90.244:5000/moscow";
-    private static final int NOTIFY_ID = 10 ;
     TextView tvRez;
     TextView tvCounter;
     int queryCounter = 0;
     ImageView iv;
-    View view;
 
     public Bitmap textAsBitmap(String text, float textSize, int textColor) {
         Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
@@ -86,50 +79,9 @@ public class MainActivity extends AppCompatActivity implements OnWebAsyncTaskCom
         Bitmap img =  textAsBitmap("22", 60, Color.RED);
         iv.setImageBitmap(img);
 
+        Notification notification = new Notification("myChnl", getApplicationContext());
 
-
-        Context context = getApplicationContext();
-        String idChannel = "my_channel_01";
-        Intent mainIntent;
-
-        mainIntent = new Intent(context, LauncherActivity.class);
-
-        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, mainIntent, 0);
-
-        NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-
-        NotificationChannel mChannel = null;
-        // The id of the channel.
-
-
-
-
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, null);
-        builder.setContentTitle(context.getString(R.string.app_name))
-                .setSmallIcon(R.drawable.ic_stat_name)
-                .setContentIntent(pendingIntent)
-                .setContentText("TEST ALARM!");
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            int importance = NotificationManager.IMPORTANCE_HIGH;
-            mChannel = new NotificationChannel(idChannel, context.getString(R.string.app_name), importance);
-            // Configure the notification channel.
-            mChannel.setDescription("CHANNEL TEST ALARM");
-            mChannel.enableLights(true);
-            mChannel.setLightColor(Color.RED);
-            mChannel.enableVibration(true);
-            mChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
-            mNotificationManager.createNotificationChannel(mChannel);
-        } else {
-            builder.setContentTitle(context.getString(R.string.app_name))
-                    .setPriority(NotificationCompat.PRIORITY_HIGH)
-                    .setColor(ContextCompat.getColor(context, R.color.colorAccent))
-                    .setVibrate(new long[]{100, 250})
-                    .setLights(Color.YELLOW, 500, 5000)
-                    .setAutoCancel(true);
-        }
-        builder.setChannelId(idChannel);
-        mNotificationManager.notify(1, builder.build());
+        notification.send("Test", img);
 
     }
 

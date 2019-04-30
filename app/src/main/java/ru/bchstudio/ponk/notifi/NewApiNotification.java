bdab4bb638c788f70847fc8c4a26306e054a98bf
Andrey Bochkarev <33877753+Vivatist@users.noreply.github.com>
2019-04-30 22:07:07 +0300

    В классах нотификации разнесены конструктор и исполняемая часть

--- app/src/main/java/ru/bchstudio/ponk/notifi/NewApiNotification.java
+++ app/src/main/java/ru/bchstudio/ponk/notifi/NewApiNotification.java
@@ -16,13 +16,30 @@ import ru.bchstudio.ponk.R;
 
 @TargetApi(26)
 public class NewApiNotification implements ApiNotification {
-
-    private Context context;
-    private String idChannel;
+    private NotificationCompat.Builder builder;
+    private NotificationManager mNotificationManager;
 
     NewApiNotification(String idChannel, Context context) {
-        this.context = context;
-        this.idChannel = idChannel;
+
+        Intent mainIntent = new Intent(context, LauncherActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, mainIntent, 0);
+
+        this.mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        int importance = NotificationManager.IMPORTANCE_HIGH;
+        NotificationChannel mChannel = new NotificationChannel(idChannel, context.getString(R.string.app_name), importance);
+        // Configure the notification channel.
+      //  mChannel.setDescription("CHANNEL TEST ALARM");
+       // mChannel.enableLights(true);
+       // mChannel.setLightColor(Color.RED);
+       // mChannel.enableVibration(true);
+       // mChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+        mNotificationManager.createNotificationChannel(mChannel);
+
+        this.builder = new NotificationCompat.Builder(context, idChannel);
+        builder.setContentTitle(context.getString(R.string.app_name))
+                .setChannelId(idChannel)
+                .setContentIntent(pendingIntent);
     }
 
 
@@ -31,34 +48,9 @@ public class NewApiNotification implements ApiNotification {
 
         Log.e(this.getClass().getSimpleName(),"Выводим сообщение в новом API");
 
-        Intent mainIntent;
-
-        mainIntent = new Intent(context, LauncherActivity.class);
-
-        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, mainIntent, 0);
-
-        NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-
-        NotificationChannel mChannel;
-        // The id of the channel.
-
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, idChannel);
-        builder.setContentTitle(context.getString(R.string.app_name))
-                .setSmallIcon(R.drawable.ic_stat_name)
-                .setContentIntent(pendingIntent)
+        builder.setSmallIcon(R.drawable.ic_stat_name)
                 .setContentText("TEST ALARM!");
 
-        int importance = NotificationManager.IMPORTANCE_HIGH;
-        mChannel = new NotificationChannel(idChannel, context.getString(R.string.app_name), importance);
-        // Configure the notification channel.
-        mChannel.setDescription("CHANNEL TEST ALARM");
-        mChannel.enableLights(true);
-        mChannel.setLightColor(Color.RED);
-        mChannel.enableVibration(true);
-        mChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
-        mNotificationManager.createNotificationChannel(mChannel);
-
-        builder.setChannelId(idChannel);
         mNotificationManager.notify(1, builder.build());
 
     }
--- app/src/main/java/ru/bchstudio/ponk/notifi/OldApiNotification.java
+++ app/src/main/java/ru/bchstudio/ponk/notifi/OldApiNotification.java
@@ -15,44 +15,36 @@ import ru.bchstudio.ponk.R;
 
 public class OldApiNotification implements ApiNotification {
 
-    private Context context;
-    private String idChannel;
+
+    private NotificationCompat.Builder builder;
+    private NotificationManager mNotificationManager;
 
     OldApiNotification(String idChannel, Context context) {
-        this.context = context;
-        this.idChannel = idChannel;
-    }
 
-    @Override
-    public void send(String text, Bitmap icon) {
-        Log.e(this.getClass().getSimpleName(),"Выводим сообщение в старом API");
-
-        Intent mainIntent;
-
-        mainIntent = new Intent(context, LauncherActivity.class);
+        Intent mainIntent = new Intent(context, LauncherActivity.class);
 
         PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, mainIntent, 0);
 
-        NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager  = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
 
-
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, idChannel);
-        builder.setContentTitle(context.getString(R.string.app_name))
-                .setSmallIcon(R.drawable.ic_stat_name)
-                .setContentIntent(pendingIntent)
-                .setContentText("TEST ALARM!");
-
+        builder = new NotificationCompat.Builder(context, idChannel);
 
         builder.setContentTitle(context.getString(R.string.app_name))
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
                 .setColor(ContextCompat.getColor(context, R.color.colorAccent))
                 .setVibrate(new long[]{100, 250})
                 .setLights(Color.YELLOW, 500, 5000)
-                .setAutoCancel(true);
+                .setAutoCancel(true).setChannelId(idChannel)
+                .setContentIntent(pendingIntent);
 
+    }
 
-        builder.setChannelId(idChannel);
+    @Override
+    public void send(String text, Bitmap icon) {
+        Log.e(this.getClass().getSimpleName(),"Выводим сообщение в старом API");
 
+        builder.setSmallIcon(R.drawable.ic_stat_name)
+                .setContentText("TEST ALARM!");
 
         mNotificationManager.notify(1, builder.build());
     }

d81f2e2e9fbaf77afd1287f29f3deafac3014d4c
Андрей Бочкарев <anvabo@gmail.com>
2019-05-24 15:16:30 +0300

    Декомпозиция. Работа с notification вынесена в отдельный класс

--- app/src/main/java/ru/bchstudio/ponk/service/BackgroundService.java
+++ app/src/main/java/ru/bchstudio/ponk/service/BackgroundService.java
@@ -2,25 +2,21 @@ package ru.bchstudio.ponk.service;
 
 import android.app.AlarmManager;
 import android.app.Application;
-import android.app.Notification;
-import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
 import android.os.Handler;
 import android.os.IBinder;
-import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import android.widget.Toast;
 
 import java.util.Calendar;
-import java.util.Date;
+
 
 import ru.bchstudio.ponk.Constants;
-import ru.bchstudio.ponk.MainActivity;
+
 import ru.bchstudio.ponk.WeatherPOJO;
 import ru.bchstudio.ponk.web.OnWebAsyncTaskCompleted;
 import ru.bchstudio.ponk.R;
@@ -31,28 +27,16 @@ public class BackgroundService extends Service implements OnWebAsyncTaskComplete
     private static final String TAG = WebAsyncTask.class.getName();
     private Thread mainThread;
     public static Intent serviceIntent = null;
-    private static final String CHANNEL_ID = "Channel ID"; //TODO придумать более удачное название
-    private static final String CHANNEL_NAME = "Channel name"; //TODO придумать более удачное название
-    private static final int NOTIFICATION_ID = 1010;
-
+    private ServiceNotificationInterface serviceNotification;
 
     public BackgroundService() {
     }
 
-    //Запускаем WatchDog
-    protected void setAlarmTimer() {
-        final Calendar c = Calendar.getInstance();
-        c.setTimeInMillis(System.currentTimeMillis());
-        c.add(Calendar.SECOND, 1);
-        Intent intent = new Intent(this, AlarmRecever.class);
-        PendingIntent sender = PendingIntent.getBroadcast(this, 0, intent, 0);
 
-        AlarmManager mAlarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
-        mAlarmManager.set(AlarmManager.RTC_WAKEUP, c.getTimeInMillis(), sender);
-    }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+
 
 
         serviceIntent = intent;
@@ -64,7 +48,9 @@ public class BackgroundService extends Service implements OnWebAsyncTaskComplete
             return START_NOT_STICKY;
         }
 
-        startForeground(NOTIFICATION_ID, prepareNotification(R.drawable.ic_stat_cloud_done, "MyTitleDone", "MyTextDone", Calendar.getInstance().getTime()));
+
+        serviceNotification = new ServiceNotification(this);
+        startForeground(serviceNotification.getId(), serviceNotification.getNotification());
 
         if (Constants.ENABLED_DEBUG_TWIST) showToast(getApplication(), "Start Foreground Service");
 
@@ -95,6 +81,20 @@ public class BackgroundService extends Service implements OnWebAsyncTaskComplete
         mainThread.start(); //Запускаем поток
 
         return START_NOT_STICKY;
+    }
+
+
+
+    //Запускаем WatchDog
+    protected void setAlarmTimer() {
+        final Calendar c = Calendar.getInstance();
+        c.setTimeInMillis(System.currentTimeMillis());
+        c.add(Calendar.SECOND, 1);
+        Intent intent = new Intent(this, AlarmRecever.class);
+        PendingIntent sender = PendingIntent.getBroadcast(this, 0, intent, 0);
+
+        AlarmManager mAlarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        mAlarmManager.set(AlarmManager.RTC_WAKEUP, c.getTimeInMillis(), sender);
     }
 
 
@@ -145,55 +145,9 @@ public class BackgroundService extends Service implements OnWebAsyncTaskComplete
     }
 
 
-    private Notification prepareNotification(int icon, String contentTitle, String contentText, Date upd_time) {
-
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID);
-        Intent notificationIntent = new Intent(this, MainActivity.class);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
-
-        builder.setSmallIcon(icon)
-                .setContentTitle(contentTitle)
-                .setContentText(contentText)
-                .setContentIntent(pendingIntent)
-                .setWhen(upd_time.getTime())
-                .setSound(null);
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-            NotificationChannel notificationChannel = new NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_LOW);
-            notificationChannel.setSound(null, null);
-            notificationChannel.setShowBadge(false);
-            manager.createNotificationChannel(notificationChannel);
-        }
-
-        return builder.build();
-
-    }
 
 
 
-    private int getIcon(int value){
-
-        String nameResource;
-        int result;
-        try {
-            if (value >= 0){
-                nameResource = "ic_degrees_"+ value;
-                return R.drawable.class.getField(nameResource).getInt(getResources());
-            } else {
-                nameResource = "ic_degrees_minus"+ Math.abs(value);
-                result = R.drawable.class.getField(nameResource).getInt(getResources());
-            }
-
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-            result =  R.drawable.ic_stat_error_outline;
-        } catch (NoSuchFieldException e) {
-            result =  R.drawable.ic_stat_error_outline;
-        }
-
-        return result;
-    }
 
 
 
@@ -203,22 +157,28 @@ public class BackgroundService extends Service implements OnWebAsyncTaskComplete
     public void onWebAsyncTaskCompleted(String result) {
 
         Log.d(TAG, "Ответ от сервера " + result);
-        Notification notification;
 
         if (result != null){
             if (Constants.ENABLED_DEBUG_TWIST) showToast(getApplication(), result);
 
             WeatherPOJO weather = new WeatherPOJO(result);
-            notification = prepareNotification(getIcon(weather.getTemp()), "Ветер " + weather.getWind_spd() + "м/с", "Влажность " + weather.getHumidity() + "%", weather.getUpd_time());
+            serviceNotification.setTemperature(weather.getTemp());
+            serviceNotification.setContentTitle("Ветер " + weather.getWind_spd() + "м/с");
+            serviceNotification.setContentText("Влажность " + weather.getHumidity() + "%");
+            serviceNotification.setUpd_time(weather.getUpd_time());
 
         } else {
             if (Constants.ENABLED_DEBUG_TWIST) showToast(getApplication(), "Ошибка соединения");
-            notification = prepareNotification(R.drawable.ic_stat_cloud_off, "Нет связи", "", Calendar.getInstance().getTime());
+            serviceNotification.setIcon(R.drawable.ic_stat_cloud_off);
+            serviceNotification.setContentTitle("Нет связи");
+            serviceNotification.setContentText("");
+            serviceNotification.setUpd_time(Calendar.getInstance().getTime());
+
 
         }
 
         NotificationManager mNotificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-        mNotificationManager.notify(NOTIFICATION_ID, notification);
+        mNotificationManager.notify(serviceNotification.getId(), serviceNotification.getNotification());
 
 
 
---
+++ app/src/main/java/ru/bchstudio/ponk/service/ServiceNotification.java
@@ -0,0 +1,129 @@
+package ru.bchstudio.ponk.service;
+
+import android.annotation.SuppressLint;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import ru.bchstudio.ponk.MainActivity;
+import ru.bchstudio.ponk.R;
+
+
+
+@SuppressLint("Registered")
+public class ServiceNotification  implements ServiceNotificationInterface {
+
+    private static final String CHANNEL_ID = "Channel ID"; //TODO придумать более удачное название
+    private static final String CHANNEL_NAME = "Channel name"; //TODO придумать более удачное название
+    private static final int NOTIFICATION_ID = 1010;
+    private Context context;
+
+
+    private int temperature = 0;
+    private int icon;
+    private String contentTitle = null;
+    private String contentText = null;
+    private Date upd_time;
+
+
+    public ServiceNotification(Context context) {
+        this.context = context;
+        this.upd_time = Calendar.getInstance().getTime();
+    }
+
+    public void setTemperature(int temperature) {
+        this.temperature = temperature;
+        this.icon = getIcon(temperature);
+    }
+
+    public void setContentTitle(String contentTitle) {
+        this.contentTitle = contentTitle;
+    }
+
+    public void setContentText(String contentText) {
+        this.contentText = contentText;
+    }
+
+    public void setUpd_time(Date upd_time) {
+        this.upd_time = upd_time;
+    }
+
+    public void setIcon(int icon){
+        this.icon = icon;
+    }
+
+    @Override
+    public Notification getNotification() {
+        return prepareNotification( context,  icon,  contentTitle,  contentText,  upd_time);
+    }
+
+
+
+
+
+
+    private Notification prepareNotification(Context context, int icon, String contentTitle, String contentText, Date upd_time) {
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID);
+        Intent notificationIntent = new Intent(context, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, notificationIntent, 0);
+
+        builder.setSmallIcon(icon)
+                .setContentTitle(contentTitle)
+                .setContentText(contentText)
+                .setContentIntent(pendingIntent)
+                .setWhen(upd_time.getTime())
+                .setSound(null);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationManager manager = (NotificationManager) context.getSystemService(context.NOTIFICATION_SERVICE);
+            NotificationChannel notificationChannel = new NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_LOW);
+            notificationChannel.setSound(null, null);
+            notificationChannel.setShowBadge(false);
+            manager.createNotificationChannel(notificationChannel);
+        }
+
+        return builder.build();
+
+    }
+
+
+    @Override
+    public int getId() {
+        return NOTIFICATION_ID;
+    }
+
+    private int getIcon(int value){
+
+        String nameResource;
+        int result;
+        try {
+            if (value >= 0){
+                nameResource = "ic_degrees_"+ value;
+                return R.drawable.class.getField(nameResource).getInt(context.getResources());
+            } else {
+                nameResource = "ic_degrees_minus"+ Math.abs(value);
+                result = R.drawable.class.getField(nameResource).getInt(context.getResources());
+            }
+
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+            result =  R.drawable.ic_stat_error_outline;
+        } catch (NoSuchFieldException e) {
+            result =  R.drawable.ic_stat_error_outline;
+        }
+
+        return result;
+    }
+
+}
